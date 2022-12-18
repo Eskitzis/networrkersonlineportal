9033a778f63ba0ab0bf34701728687ec5c3eeadb
eskizis <antonis-9@live.de>
2022-12-18 22:13:02 +0200

    commit

--- index.php
+++ index.php
@@ -5,14 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" type="text/css" href="loginstyle/vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="loginstyle/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="loginstyle/vendor/animate/animate.css">
-    <link rel="stylesheet" type="text/css" href="loginstyle/vendor/css-hamburgers/hamburgers.min.css">
-    <link rel="stylesheet" type="text/css" href="loginstyle/vendor/select2/select2.min.css">
-    <link rel="stylesheet" type="text/css" href="loginstyle/css/util.css">
-	<link rel="stylesheet" type="text/css" href="loginstyle/css/main.css">
-    <link rel="stylesheet" href="https://peaceful-hypatia.185-207-107-69.plesk.page/styles/loginstyle.css">
+    <!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="styles/util.css">
+	<link rel="stylesheet" type="text/css" href="styles/main.css">
+    <!--===============================================================================================-->
     <link rel="icon" href="assets/logo.png">
     <title>Networrkers</title>
 </head>
