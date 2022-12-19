d00a10f5af655c7c7ccf5c0dcf7238243e67b40c
eskizis <antonis-9@live.de>
2022-12-19 03:34:49 +0200

    aa

--- process.php
+++ process.php
@@ -6,12 +6,6 @@
     //prevent injections
     $username = stripcslashes($username);
     $password = stripcslashes($password);
-    //$username = mysql_real_escape_string($username);
-    //$password = mysql_real_escape_string($password);
-
-    // connect to the server and select database
-    //mysql_connect("localhost", "root", "");
-    //mysql_select_db("login");
 
     $con = mysqli_connect('v2202208181594198845.nicesrv.de', 'nassos', 'nassos1106', 'users');
 
@@ -20,41 +14,30 @@
               or die("Failed to query database".mysql_error());
     $row = mysqli_fetch_array($result, MYSQLI_BOTH);
 
-    /*
-    // Check connection
-    if ($con->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
-    */
-
     if ($row['username'] == $username && $row['password'] == $password && $row['role'] == "employee"){
         //echo "Login Success!!! Welcome ".$row['username']." ".$row['role'];
-        header("Location: employee.php");
         session_start();
+        header("Location: employee.php");
         $_SESSION['fname'] = $row['fullname'];
         $_SESSION['url'] = $row['caldav'];
         //$_SESSION['sk'] = $row['skill'];
     }
-    else echo "Not";
 
     if ($row['username'] == $username && $row['password'] == $password && $row['role'] == "teamleader"){
         //echo "Login Success!!! Welcome ".$row['username']." ".$row['role'];
-        header("Location: teamleader.php");
         session_start();
+        header("Location: teamleader.php");
         $_SESSION['fname'] = $row['fullname'];
         $_SESSION['url'] = $row['caldav'];
         //$_SESSION['sk'] = $row['skill'];
     }
-    else echo "Not";
 
     if ($row['username'] == $username && $row['password'] == $password && $row['role'] == "admin"){
         //echo "Login Success!!! Welcome ".$row['username']." ".$row['role'];
-        header("Location: admin.php");
         session_start();
+        header("Location: admin.php");
         $_SESSION['fname'] = $row['fullname'];
         $_SESSION['url'] = $row['caldav'];
         //$_SESSION['sk'] = $row['skill'];
     }
-    else echo "Not";
 ?>
