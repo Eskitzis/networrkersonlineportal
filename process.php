528ef92fd370a719f23111176f139fc40a8a3fcf
eskizis <antonis-9@live.de>
2022-12-19 03:14:05 +0200

    ccccc

--- process.php
+++ process.php
@@ -1,41 +1,48 @@
 <?php
-    session_start(); 
+    //values passed from index form
     $username = $_POST['user'];
     $password = $_POST['pass'];
 
     //prevent injections
-    //$username = stripcslashes($username);
-    //$password = stripcslashes($password);
+    $username = stripcslashes($username);
+    $password = stripcslashes($password);
     //$username = mysql_real_escape_string($username);
     //$password = mysql_real_escape_string($password);
 
-    $conn = mysqli_connect('v2202208181594198845.nicesrv.de', 'nassos', 'nassos1106', 'users');
+    // connect to the server and select database
+    //mysql_connect("localhost", "root", "");
+    //mysql_select_db("login");
 
-    /*if ($con->connect_error) {
-        die("Connection failed: " . $con->connect_error);
-      }
-      echo "Connected successfully";
-    */
+    $con = mysqli_connect('v2202208181594198845.nicesrv.de', 'nassos', 'nassos1106', 'users');
 
     //Query the database for user
-    $result = mysqli_query($conn, "select * from networrkers where username = '$username' and password = '$password'") 
+    $result = mysqli_query($con, "select * from networrkers where username = '$username' and password = '$password'") 
               or die("Failed to query database".mysql_error());
     $row = mysqli_fetch_array($result, MYSQLI_BOTH);
 
+    if ($row['username'] == $username && $row['password'] == $password && $row['role'] == "employee"){
+        //echo "Login Success!!! Welcome ".$row['username']." ".$row['role'];
+        header("Location: employee.php");
+        session_start();
+        //$_SESSION['fname'] = $row['fullname'];
+        //$_SESSION['url'] = $row['caldav'];
+        //$_SESSION['sk'] = $row['skill'];
+    }
+    if ($row['username'] == $username && $row['password'] == $password && $row['role'] == "teamleader"){
+        //echo "Login Success!!! Welcome ".$row['username']." ".$row['role'];
+        header("Location: teamleader.php");
+        session_start();
+        //$_SESSION['fname'] = $row['fullname'];
+        //$_SESSION['url'] = $row['caldav'];
+        //$_SESSION['sk'] = $row['skill'];
+    }
+    if ($row['username'] == $username && $row['password'] == $password && $row['role'] == "admin"){
+        //echo "Login Success!!! Welcome ".$row['username']." ".$row['role'];
+        header("Location: admin.php");
+        session_start();
+        //$_SESSION['fname'] = $row['fullname'];
+        //$_SESSION['url'] = $row['caldav'];
+        //$_SESSION['sk'] = $row['skill'];
+    }
 
-    if ($row['role'] == "employee"){
-        echo "Login Success!!! Welcome ".$row['username']." ".$row['role'];
-        //header("Location: employee.php");
-        session_start();
-    }
-    if ($row['role'] == "teamleader"){
-        echo "Login Success!!! Welcome ".$row['username']." ".$row['role'];
-        //header("Location: teamleader.php");
-        session_start();
-    }
-    if ($row['role'] == "admin"){
-        echo "Login Success!!! Welcome ".$row['username']." ".$row['role'];
-        //header("Location: admin.php");
-        session_start();
-    }
 ?>
