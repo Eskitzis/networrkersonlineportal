0ef140182af123c00705218792f65dbe739d8bd5
eskizis <antonis-9@live.de>
2022-12-19 04:27:34 +0200

    cndfgn

--- admin.php
+++ admin.php
@@ -18,7 +18,6 @@
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
-
     <script src="js/admin.js"></script>
     <title>Admin</title>
 </head>
@@ -50,6 +49,5 @@
     </nav>
 </div>
 <!--//////////////////////////////////////////NAVBAR END//////////////////////////////////////////-->
-
 </body>
 </html>
