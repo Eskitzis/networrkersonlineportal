64d2d9ebc6871958b254f121940638d488209007
eskizis <antonis-9@live.de>
2022-12-22 17:10:16 +0200

    commit

--- admin_database.php
+++ admin_database.php
@@ -17,7 +17,7 @@
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
     <script src="js/admin.js"></script>
     <title>Admin DB</title>
 </head>
