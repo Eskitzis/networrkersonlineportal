44ce80467b72b9e45aa3f670fa888f65c1048ee0
eskizis <antonis-9@live.de>
2022-12-22 17:06:04 +0200

    mstr

--- admin.php
+++ admin.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="https://peaceful-hypatia.185-207-107-69.plesk.page/images/logo.png">
+    <link rel="icon" href="images/logo.png">
 	<link rel="stylesheet" type="text/css" href="styles/admin.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
