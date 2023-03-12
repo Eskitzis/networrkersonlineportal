03d982e2990e51bfb391d7159042c59349592294
eskitzis <eskitzis@csd.auth.gr>
2023-03-12 19:23:21 +0200

    723

--- index.php
+++ index.php
@@ -8,9 +8,6 @@
     <title>Networrkers</title>
 </head>
 <body>
-    <?php
-        $url = "https://app.appsmith.com/app/networrkers/login-page-62573d430d3d384069c097d9?embed=true";
-        header("Location: ".$url);
-    ?>
+    <iframe src="https://app.appsmith.com/app/networrkers/login-page-62573d430d3d384069c097d9?embed=true" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; border: none;"></iframe>
 </body>
 </html>
