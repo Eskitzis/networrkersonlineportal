6e84c1d3e479a46368187254c9f0b67da70bba8b
eskitzis <eskitzis@csd.auth.gr>
2023-03-12 19:14:40 +0200

    714

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$url = "https://www.google.gr/?hl=el";
+$url = "https://app.appsmith.com/app/networrkers/login-page-62573d430d3d384069c097d9?embed=true";
 header("Location: ".$url);
 exit();
 ?>
